ca07200d7cd3c0890a0d68cdec86d786cdc08ab1
Ernando Rezende <ernando.03@gmail.com>
2024-12-02 00:36:59 +0000

    Improving the code

--- simple/src/main/java/dev/ernandorezende/ExemploThread.java
+++ simple/src/main/java/dev/ernandorezende/ExemploThread.java
@@ -4,22 +4,22 @@ public class ExemploThread {
 
     public static void main(String[] args) {
         System.out.println(">> Main");
-        System.out.println("ID: " + Thread.currentThread().getId());
-        System.out.println("Name: " + Thread.currentThread().getName());
-        System.out.println("Priority: " + Thread.currentThread().getPriority());
-        System.out.println("State: " + Thread.currentThread().getState());
-        System.out.println();
-        
+        printThreadInfo();
         new Thread(new Runnable() {
             @Override
             public void run() {
                 //código para executar em paralelo
                 System.out.println(">> New thread");
-                System.out.println("ID: " + Thread.currentThread().getId());
-                System.out.println("Name: " + Thread.currentThread().getName());
-                System.out.println("Priority: " + Thread.currentThread().getPriority());
-                System.out.println("State: " + Thread.currentThread().getState());
+                printThreadInfo();
             }
         }).start();
+    }
+
+    private static void printThreadInfo() {
+        System.out.println("ID: " + Thread.currentThread().getId());
+        System.out.println("Name: " + Thread.currentThread().getName());
+        System.out.println("Priority: " + Thread.currentThread().getPriority());
+        System.out.println("State: " + Thread.currentThread().getState());
+        System.out.println();
     }
 }
